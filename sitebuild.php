94b20b8bca0e4c724bc435c2c9551501ec638f73
OverdueWeevil2 <67264530+OverdueWeevil2@users.noreply.github.com>
2022-02-16 15:50:25 +0300

    Create sitebuild.php

--- sitebuild.php
+++ sitebuild.php
@@ -6,8 +6,11 @@ require_once __DIR__.'/handler/AreaHandler.php';
 require_once __DIR__.'/includes/Header.php';
 require_once __DIR__.'/handler/CookieHandler.php';
 require_once __DIR__.'/handler/ValidationHandler.php';
+require_once __DIR__.'/handler/HostingHandler.php';
 require_once __DIR__.'/modules/autoload.php';
 require_once __DIR__.'/modules/UserInfo/UserInfo.php';
+require_once __DIR__.'/includes/Navbar.php';
+require_once __DIR__.'/includes/Sidebar.php';
 $sql = mysqli_query($connect,"SELECT * FROM `hosting_account` WHERE `account_username`='".$_GET['username']."' AND `account_for`='".$ClientInfo['hosting_client_key']."'");
 if(mysqli_num_rows($sql)>0){
 	echo "";
