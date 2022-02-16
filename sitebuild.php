76ebe493ae5580e08a69eae538a36e916486056b
OverdueWeevil2 <67264530+OverdueWeevil2@users.noreply.github.com>
2022-02-16 15:09:41 +0300

    Update sitebuild.php

--- sitebuild.php
+++ sitebuild.php
@@ -8,8 +8,6 @@ require_once __DIR__.'/includes/Connect.php';
 require_once __DIR__.'/handler/AreaHandler.php';
 require_once __DIR__.'/handler/CookieHandler.php';
 require_once __DIR__.'/handler/ValidationHandler.php';
-require_once __DIR__.'/includes/Navbar.php';
-require_once __DIR__.'/includes/Sidebar.php';
 $sql = mysqli_query($connect,"SELECT * FROM `hosting_account` WHERE `account_username`='".$_GET['username']."' AND `account_for`='".$ClientInfo['hosting_client_key']."'");
 if(mysqli_num_rows($sql)>0){
 	echo "";
