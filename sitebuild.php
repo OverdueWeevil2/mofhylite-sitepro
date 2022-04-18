6ab070d7ede432ea45f290f2bc6693a1430a4f80
OverdueWeevil2 <67264530+OverdueWeevil2@users.noreply.github.com>
2022-04-18 23:24:18 +0300

    Update sitebuild.php

--- sitebuild.php
+++ sitebuild.php
@@ -11,12 +11,16 @@ require_once __DIR__.'/modules/autoload.php';
 require_once __DIR__.'/modules/UserInfo/UserInfo.php';
 require_once __DIR__.'/includes/Navbar.php';
 require_once __DIR__.'/includes/Sidebar.php';
-$sql = mysqli_query($connect,"SELECT * FROM `hosting_account` WHERE `account_username`='".$_GET['username']."' AND `account_for`='".$ClientInfo['hosting_client_key']."'");
+$sql = mysqli_query($connect,"SELECT * FROM `hosting_account` WHERE `account_username`='".$connect->real_escape_string($_GET['username'])."' AND `account_for`='".$ClientInfo['hosting_client_key']."'");
 if(mysqli_num_rows($sql)>0){
-	echo "";
 }
 else{
 	die("You are not authorized to access this page.");
+}
+?>
+<?php
+if(!$AccountInfo['account_status']=='1'){
+die('Your account status is NOT Active!');
 }
 ?>
 <form action="site.php" method="POST">
@@ -45,6 +49,6 @@ foreach($res as $domain){
 </select>
 <br></br>
 <input type="submit" value="Go to Site Builder"></input>
-<input id="username" name="username" value="<?php echo $_GET['username']; ?>" hidden></input>
+<input id="username" name="username" value="<?php echo htmlentities($_GET['username'], ENT_QUOTES); ?>" hidden></input>
 </form>
 <?php require_once __DIR__.'/includes/Footer.php'; ?>
